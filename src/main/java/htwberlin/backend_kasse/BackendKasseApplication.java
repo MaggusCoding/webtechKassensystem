5a812badad542bf140a3102b72cc8b2396645908
murks1 <mseethaler98@gmail.com>
2023-04-11 12:16:16 +0200

    first commit / generated project

---
+++ src/main/java/htwberlin/backend_kasse/BackendKasseApplication.java
@@ -0,0 +1,13 @@
+package htwberlin.backend_kasse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendKasseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendKasseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/htwberlin/backend_kasse/BackendKasseApplicationTests.java
@@ -0,0 +1,13 @@
+package htwberlin.backend_kasse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendKasseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
