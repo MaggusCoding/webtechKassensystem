8f5137dba627079e2599cb25a4971905d7dbf6e8
MaggusCoding <mseethaler98@gmail.com>
2023-06-15 11:28:29 +0200

    added google calendar api

--- src/main/java/htwberlin/backend_kasse/BackendKasseApplication.java
+++ src/main/java/htwberlin/backend_kasse/BackendKasseApplication.java
@@ -2,19 +2,26 @@ package htwberlin.backend_kasse;
 
 import java.util.Arrays;
 
+import com.google.api.client.json.JsonFactory;
+import com.google.api.client.json.gson.GsonFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
-@SpringBootApplication(scanBasePackages = {"com.google.api.client.json"})
+@SpringBootApplication
 public class BackendKasseApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BackendKasseApplication.class, args);
 	}
 
+	@Bean
+	public JsonFactory jsonFactory(){
+		return GsonFactory.getDefaultInstance();
+	}
 	@Bean
 	public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
 		return args -> {
---
+++ src/main/java/htwberlin/backend_kasse/services/CalendarService.java
@@ -0,0 +1,108 @@
+package htwberlin.backend_kasse.services;
+
+import com.google.api.client.auth.oauth2.Credential;
+import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInstalledApp;
+import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver;
+import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
+import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
+import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
+import com.google.api.client.http.javanet.NetHttpTransport;
+import com.google.api.client.json.JsonFactory;
+import com.google.api.client.json.gson.GsonFactory;
+import com.google.api.client.util.store.FileDataStoreFactory;
+import com.google.api.services.calendar.Calendar;
+import com.google.api.services.calendar.CalendarScopes;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.security.GeneralSecurityException;
+import java.util.Collections;
+import java.util.List;
+
+@Service
+public class CalendarService {
+
+    /**
+     * Global instance of the JSON factory.
+     */
+
+    private final JsonFactory JSON_FACTORY = GsonFactory.getDefaultInstance();
+    /**
+     * Directory to store authorization tokens for this application.
+     */
+    private final String TOKENS_DIRECTORY_PATH = "tokens";
+
+    /**
+     * Global instance of the scopes required by this quickstart.
+     * If modifying these scopes, delete your previously saved tokens/ folder.
+     */
+
+    private List<String> SCOPES =
+            Collections.singletonList(CalendarScopes.CALENDAR);
+    private final String CREDENTIALS_FILE_PATH = "/credentials.json";
+
+
+    public CalendarService() throws GeneralSecurityException, IOException {
+    }
+
+    public Calendar buildCalendar() throws IOException, GeneralSecurityException {
+        final NetHttpTransport HTTP_TRANSPORT = GoogleNetHttpTransport.newTrustedTransport();
+        return new Calendar.Builder(HTTP_TRANSPORT, JSON_FACTORY, getCredentials(HTTP_TRANSPORT))
+                .setApplicationName("BackendKasseApplication")
+                .build();
+    }
+
+    /**
+     * Creates an authorized Credential object.
+     *
+     * @param HTTP_TRANSPORT The network HTTP Transport.
+     * @return An authorized Credential object.
+     * @throws IOException If the credentials.json file cannot be found.
+     */
+    private Credential getCredentials(final NetHttpTransport HTTP_TRANSPORT)
+            throws IOException {
+        // Load client secrets.
+        InputStream in = CalendarService.class.getResourceAsStream(CREDENTIALS_FILE_PATH);
+        if (in == null) {
+            throw new FileNotFoundException("Resource not found: " + CREDENTIALS_FILE_PATH);
+        }
+        GoogleClientSecrets clientSecrets =
+                GoogleClientSecrets.load(JSON_FACTORY, new InputStreamReader(in));
+
+        // Build flow and trigger user authorization request.
+        GoogleAuthorizationCodeFlow flow = new GoogleAuthorizationCodeFlow.Builder(
+                HTTP_TRANSPORT, JSON_FACTORY, clientSecrets, SCOPES)
+                .setDataStoreFactory(new FileDataStoreFactory(new java.io.File(TOKENS_DIRECTORY_PATH)))
+                .setAccessType("offline")
+                .build();
+        LocalServerReceiver receiver = new LocalServerReceiver.Builder().setPort(8888).build();
+        Credential credential = new AuthorizationCodeInstalledApp(flow, receiver).authorize("user");
+        //returns an authorized Credential object.
+        return credential;
+    }
+
+    public JsonFactory getJSON_FACTORY() {
+        return JSON_FACTORY;
+    }
+
+    public String getTOKENS_DIRECTORY_PATH() {
+        return TOKENS_DIRECTORY_PATH;
+    }
+
+    public List<String> getSCOPES() {
+        return SCOPES;
+    }
+
+    public void setSCOPES(List<String> SCOPES) {
+        this.SCOPES = SCOPES;
+    }
+
+    public String getCREDENTIALS_FILE_PATH() {
+        return CREDENTIALS_FILE_PATH;
+    }
+
+}
