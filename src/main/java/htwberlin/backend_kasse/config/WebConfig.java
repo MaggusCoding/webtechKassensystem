2222d14e7f27d3fa01a988eda1c2a6c370756a5b
murks1 <mseethaler98@gmail.com>
2023-06-27 14:52:20 +0200

    reconfigered repo

--- src/main/java/htwberlin/backend_kasse/config/WebConfig.java
+++ src/main/java/htwberlin/backend_kasse/config/WebConfig.java
@@ -1,9 +1,16 @@
 package htwberlin.backend_kasse.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @EnableWebMvc
@@ -15,4 +22,5 @@ public class WebConfig implements WebMvcConfigurer {
                 .allowedMethods("*")
                 .allowedOrigins("http://localhost:3000");
     }
+
 }
