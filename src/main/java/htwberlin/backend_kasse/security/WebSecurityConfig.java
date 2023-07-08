b67053c4f74a885c858495e0ad72f7a6d6b5fe70
MaggusCoding <mseethaler98@gmail.com>
2023-07-08 14:20:00 +0200

    disable security test

--- src/main/java/htwberlin/backend_kasse/security/WebSecurityConfig.java
+++ src/main/java/htwberlin/backend_kasse/security/WebSecurityConfig.java
@@ -57,8 +57,8 @@ public class WebSecurityConfig {
                 .exceptionHandling(exception -> exception.authenticationEntryPoint(unauthorizedHandler))
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth ->
-                        auth.requestMatchers("/api/auth/**").permitAll()
-                                //.requestMatchers("/api/mitarbeiter").permitAll()
+                        auth.requestMatchers("/api/auth/signin").permitAll()
+                                .requestMatchers("/api/**").permitAll()
                                 .anyRequest().authenticated()
                 );
         http.cors();
